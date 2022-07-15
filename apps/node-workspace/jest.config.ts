7eff69f96e46640fc90a1d19c21b0f03c372c312
Avinash Mudunuri <avinashmudunuri@gmail.com>
2022-07-15 23:56:55 +0530

    initial commit

---
+++ apps/node-workspace/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'node-workspace',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/node-workspace',
+};
